4e2f19df8a509f45a19cb5a040c9d759b01f8bc0
Cbeaucl <chris.beauclair@gmail.com>
2015-02-05 09:19:29 -0500

    Removed Release folder from repo

--- Release/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS := C:/workspaceMars/FreeRTOS801/Release/libFreeRTOS801.a
-
-LIBS := -lm -lFreeRTOS801
-
