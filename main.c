0daa78ff2cccb9ad8de76a705d67f8dd92cc42cb
Cbeaucl <chris.beauclair@gmail.com>
2015-02-19 20:52:22 -0500

    Made changes to the I2C registers, also added in busy waits to make sure there is a good time between I2C calls.

--- main.c
+++ main.c
@@ -15,6 +15,7 @@
 #include "task.h"
 #include "queue.h"
 #include "semphr.h"
+#include "i2cMultiMaster.h"
 
 /* serial interface include file. */
 #include "serial.h"
@@ -31,56 +32,23 @@ extern xComPortHandle xSerialPort;
 
 /* Main program loop */
 int main(void) __attribute__((OS_main));
-void TaskStartIFS(uint8_t * TArray) // Main Red LED Flash
+void TaskStartIFS(void *pvParameters) // Main Red LED Flash
 {
-	while(1){
-	uint8_t transcieverMsg[2];
-	uint8_t grossDataTPA81[18];
+    (void) pvParameters;;
+	taskDISABLE_INTERRUPTS();
 
-	for (uint8_t i = 0x00; i < 0x09; i++){
-		transcieverMsg[0] = 0xD0;
-		transcieverMsg[1] = i + 1;
-
-		I2C_Master_Start_Transceiver_With_Data(&transcieverMsg[0], 2);
-		avrSerialPrint_P(PSTR("TaskReadTemperature: Start \n"));
-
-		transcieverMsg[0] = 0x01;
-		I2C_Master_Start_Transceiver_With_Data(&transcieverMsg[0], 1);
-		avrSerialPrint_P(PSTR("TaskReadTemperature: Restart \n"));
-
-		uint8_t ok = I2C_Master_Get_Data_From_Transceiver(&grossDataTPA81[2*1], (uint8_t) 2);
-		avrSerialPrint_P(PSTR("TaskReadTemperature: Read \n"));
-
-		if (ok != 1){
-			avrSerialPrint_P(PSTR("Couldn't read data \n"));
-		}
-	}
-
-	for (int j = 0; j < 18; j++){
-		if (j%2 != 0){
-			TArray[j/2] = grossDataTPA81[j];
-		}
-	}
-
-	for (int k = 0; k < 9; k++){
-		avrSerialPrintf_P(PSTR("%d"), TArray[k]);
-	}
-
-
-    //(void) pvParameters;;
-	//taskDISABLE_INTERRUPTS();
-
-    //TickType_t xLastWakeTime;
+    TickType_t xLastWakeTime;
 	/* Variable used in vTaskDelayUntil. Needs to be set once to allow for a valid start
 		time for the vTaskDelayUntil. After this point, vTaskDelayUntil handles itself. */
 	xLastWakeTime = xTaskGetTickCount();
 	I2C_Master_Initialise(0xD0);
 			taskENABLE_INTERRUPTS();
 			avrSerialPrint_P(PSTR("Entering Loop \n"));
+			I2C_Check_Free_After_Stop();
 			uint8_t arguments[3] = {0xD0, 0x00, 0x20};
 			avrSerialPrint_P(PSTR("INtiliasing arguments \n"));
 			avrSerialPrint_P(PSTR("Send write arguments \n"));
-			I2C_Master_Start_Transceiver_With_Data(arguments, 2);
+			I2C_Master_Start_Transceiver_With_Data(arguments, 3);
 
 	xSerial1Port = xSerialPortInitMinimal( USART1, 9600, portSERIAL_BUFFER_TX, portSERIAL_BUFFER_RX); //  serial port: WantedBaud, TxQueueLength, RxQueueLength (8n1)
 	char * txt = "124";
