7a1caac62e5cf8c068624bc422c849b3b0c665e0
yanko06 <yannismassylhaffaf@yahoo.fr>
2015-02-26 09:38:55 -0500

    More detailed implementation of I2C

--- main.c
+++ main.c
@@ -31,12 +31,46 @@ extern xComPortHandle xSerialPort;
 
 /* Main program loop */
 int main(void) __attribute__((OS_main));
-void TaskStartIFS(void *pvParameters) // Main Red LED Flash
+void TaskStartIFS(uint8_t * TArray) // Main Red LED Flash
 {
-    (void) pvParameters;;
-	taskDISABLE_INTERRUPTS();
+	while(1){
+	uint8_t transcieverMsg[2];
+	uint8_t grossDataTPA81[18];
 
-    TickType_t xLastWakeTime;
+	for (uint8_t i = 0x00; i < 0x09; i++){
+		transcieverMsg[0] = 0xD0;
+		transcieverMsg[1] = i + 1;
+
+		I2C_Master_Start_Transceiver_With_Data(&transcieverMsg[0], 2);
+		avrSerialPrint_P(PSTR("TaskReadTemperature: Start \n"));
+
+		transcieverMsg[0] = 0x01;
+		I2C_Master_Start_Transceiver_With_Data(&transcieverMsg[0], 1);
+		avrSerialPrint_P(PSTR("TaskReadTemperature: Restart \n"));
+
+		uint8_t ok = I2C_Master_Get_Data_From_Transceiver(&grossDataTPA81[2*1], (uint8_t) 2);
+		avrSerialPrint_P(PSTR("TaskReadTemperature: Read \n"));
+
+		if (ok != 1){
+			avrSerialPrint_P(PSTR("Couldn't read data \n"));
+		}
+	}
+
+	for (int j = 0; j < 18; j++){
+		if (j%2 != 0){
+			TArray[j/2] = grossDataTPA81[j];
+		}
+	}
+
+	for (int k = 0; k < 9; k++){
+		avrSerialPrintf_P(PSTR("%d"), TArray[k]);
+	}
+
+
+    //(void) pvParameters;;
+	//taskDISABLE_INTERRUPTS();
+
+    //TickType_t xLastWakeTime;
 	/* Variable used in vTaskDelayUntil. Needs to be set once to allow for a valid start
 		time for the vTaskDelayUntil. After this point, vTaskDelayUntil handles itself. */
 	xLastWakeTime = xTaskGetTickCount();
