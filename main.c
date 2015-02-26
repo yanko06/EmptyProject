b532c5b023b2640e8f64e0bbc339c62e7ff74ef9
yanko06 <yannismassylhaffaf@yahoo.fr>
2015-02-26 09:38:55 -0500

    More detailed implementation of I2C

--- main.c
+++ main.c
@@ -31,15 +31,49 @@ extern xComPortHandle xSerialPort;
 
 /* Main program loop */
 int main(void) __attribute__((OS_main));
-void TaskStartIFS(void *pvParameters) // Main Red LED Flash
+void TaskStartIFS(uint8_t * TArray) // Main Red LED Flash
 {
-    (void) pvParameters;;
-	taskDISABLE_INTERRUPTS();
+	while(1){
+	uint8_t transcieverMsg[2];
+	uint8_t grossDataTPA81[18];
 
-    TickType_t xLastWakeTime;
+	for (uint8_t i = 0x00; i < 0x09; i++){
+		transcieverMsg[0] = 0xD0;
+		transcieverMsg[1] = i + 1;
+
+		I2C_Master_Start_Transceiver_With_Data(&transcieverMsg[0], 2);
+		avrSerialPrint_P(PSTR("TaskReadTemperature: Start \n"));
+
+		transcieverMsg[0] = 0x01;
+		I2C_Master_Start_Transceiver_With_Data(&transcieverMsg[0], 1);
+		avrSerialPrint_P(PSTR("TaskReadTemperature: Restart \n"));
+
+		uint8_t ok = I2C_Master_Get_Data_From_Transceiver(&grossDataTPA81[2*1], (uint8_t) 2);
+		avrSerialPrint_P(PSTR("TaskReadTemperature: Read \n"));
+
+		if (ok != 1){
+			avrSerialPrint_P(PSTR("Couldn't read data \n"));
+		}
+	}
+
+	for (int j = 0; j < 18; j++){
+		if (j%2 != 0){
+			TArray[j/2] = grossDataTPA81[j];
+		}
+	}
+
+	for (int k = 0; k < 9; k++){
+		avrSerialPrintf_P(PSTR("%d"), TArray[k]);
+	}
+
+
+    //(void) pvParameters;;
+	//taskDISABLE_INTERRUPTS();
+
+    //TickType_t xLastWakeTime;
 	/* Variable used in vTaskDelayUntil. Needs to be set once to allow for a valid start
 		time for the vTaskDelayUntil. After this point, vTaskDelayUntil handles itself. */
-	xLastWakeTime = xTaskGetTickCount();
+	/**xLastWakeTime = xTaskGetTickCount();
 	I2C_Master_Initialise(0xD0);
 			taskENABLE_INTERRUPTS();
 			avrSerialPrint_P(PSTR("Entering Loop \n"));
@@ -62,10 +96,11 @@ void TaskStartIFS(void *pvParameters) // Main Red LED Flash
 		uint8_t message;
 		avrSerialPrint_P(PSTR("Attempt to read data from thingy \n"));
 		I2C_Master_Get_Data_From_Transceiver(&message, 24);
-		avrSerialPrint_p(message);
+		avrSerialPrint_P(message);
 		avrSerialPrint_P(PSTR("message \n"));
-		}
+		}*/
 
+}
 }
 int main(void)
 {
@@ -73,11 +108,13 @@ int main(void)
 	xSerialPort = xSerialPortInitMinimal( USART0, 115200, portSERIAL_BUFFER_TX, portSERIAL_BUFFER_RX); //  serial port: WantedBaud, TxQueueLength, RxQueueLength (8n1)
 	avrSerialPrint_P(PSTR("\r\n\n\nHello World!\r\n"));
 
+	uint8_t TArray[9];
+	//uint8_t *arrayPointer = TArray;
 	xTaskCreate(
 			TaskStartIFS
 			,  (const portCHAR *)"IFS"
 			,  256
-			,  NULL
+			,  &TArray
 			,  3
 			,  NULL );
 
