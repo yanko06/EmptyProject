06db1ca5f7c9bacce45815d2cdbe5efdf94aa77e
Cbeaucl <chris.beauclair@gmail.com>
2015-02-27 14:33:32 -0500

    Fixing little issues with merge

--- main.c
+++ main.c
@@ -32,9 +32,40 @@ extern xComPortHandle xSerialPort;
 
 /* Main program loop */
 int main(void) __attribute__((OS_main));
+void taskForOrdancing(void *pvParameters) // Main Red LED Flash
+{
+
+
+}
+}
+void TaskPrintToUsart(void *pvParameters) // Main Red LED Flash
+{
+	TickType_t xLastWakeTime;
+/* Variable used in vTaskDelayUntil. Needs to be set once to allow for a valid start
+	time for the vTaskDelayUntil. After this point, vTaskDelayUntil handles itself. */
+	xLastWakeTime = xTaskGetTickCount();
+	xSerialPort = xSerialPortInitMinimal( USART1, 9600, portSERIAL_BUFFER_TX, portSERIAL_BUFFER_RX); //  serial port: WantedBaud, TxQueueLength, RxQueueLength (8n1)
+
+	xSerial1Port = xSerialPortInitMinimal( USART1, 9600, portSERIAL_BUFFER_TX, portSERIAL_BUFFER_RX); //  serial port: WantedBaud, TxQueueLength, RxQueueLength (8n1)
+	char * txt = "124";
+	//char pointerTo = &txt;
+	uint8_t * cvn = (uint8_t *) txt;
+	xComPortHandlePtr xSerialPortPtr = &xSerial1Port;
+
+
+    while(1)
+    {
+    	xSerialFlush(xSerialPortPtr);
+    	xSerialxPrint(xSerialPortPtr, cvn);
+
+    	//avrSerialPrint(cvn);
+		vTaskDelayUntil( &xLastWakeTime, ( 1750 / portTICK_PERIOD_MS ) );
+    }
+}
+
 int main(void)
 {
-	/*taskDISABLE_INTERRUPTS();
+
     // turn on the serial port for debugging or for other USART reasons.
 	xSerialPort = xSerialPortInitMinimal( USART1, 9600, portSERIAL_BUFFER_TX, portSERIAL_BUFFER_RX); //  serial port: WantedBaud, TxQueueLength, RxQueueLength (8n1)
 	avrSerialPrint_P(PSTR("\r\n\n\nHello World!\r\n"));
@@ -49,16 +80,6 @@ int main(void)
 			,  NULL
 			,  3
 			,  NULL );
-
-    xTaskCreate(
-    		TaskPrintToUsart
-		,  (const portCHAR *)"USART"
-		,  256				
-		,  NULL
-		,  3
-		,  NULL ); 
-
-
 
 	vTaskStartScheduler();
 
